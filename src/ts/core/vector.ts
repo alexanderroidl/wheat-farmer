a71a75e98b16df7e7f3bc5d8a312177006575d0e
Alexander Roidl <alexanderroidl@gmail.com>
2021-05-04 14:09:53 +0200

    Added doc comments to Vector class

--- src/ts/core/vector.ts
+++ src/ts/core/vector.ts
@@ -2,20 +2,61 @@ export default class Vector {
     public x: number;
     public y: number;
 
+    /**
+     * Retrieve total length
+     * 
+     * @returns Total length
+     */
     get length (): number {
         return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
     }
 
+    /**
+     * Constructor
+     * 
+     * @param x - Initial x coordinate
+     * @param y - Initial y coordinate
+     */
     constructor (x: number, y: number) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Add to coordinates
+     * 
+     * @param x - Added to x coordinate
+     * @param y - Added to y coordinate
+     * @returns Vector with added coordinates
+     */
     public add (x: number, y: number): Vector {
         return new Vector(this.x + x, this.y + y)
     }
 
+    /**
+     * Round off coordinates
+     * 
+     * @returns Vector with rounded off coordinates
+     */
     public floor (): Vector {
         return new Vector(Math.floor(this.x), Math.floor(this.y));
+    }
+
+    /**
+     * Rotate clockwise by degrees
+     * 
+     * @param deg - Angle in degrees
+     * @returns Rotated vector
+     */
+    public rotate (deg: number): Vector {
+        const rad = -deg * (Math.PI/180);
+
+        const cos = Math.cos(rad);
+        const sin = Math.sin(rad);
+
+        return new Vector(
+            Math.round(10000 * (this.x * cos - this.y * sin)) / 10000, 
+            Math.round(10000 * (this.x * sin + this.y * cos)) / 10000
+        );
     }
 }
