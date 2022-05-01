704034c4d80cef3397bcd0eaa24c486445eeddef
Alexander Roidl <aroidl@crusoemedia.com>
2022-05-01 17:32:43 +0200

    Fixed ESLint errors Built everything

--- project/src/ts/core/game-loop.ts
+++ project/src/ts/core/game-loop.ts
@@ -1,107 +1,107 @@
 export default class GameLoop {
-    public simulationStep = 1000 / 60;
-    public frameDelta = 0;
-    public lastFrameTimeMs = 0;
-    public fps = 60;
-    public lastFpsUpdate = 0;
-    public framesThisSecond = 0;
-    public numUpdateSteps = 0;
-    public minFrameDelay = 0;
-    public running = false;
-    public started = false;
-    public panic = false;
-    public rafHandle?: number;
+  public simulationStep = 1000 / 60;
+  public frameDelta = 0;
+  public lastFrameTimeMs = 0;
+  public fps = 60;
+  public lastFpsUpdate = 0;
+  public framesThisSecond = 0;
+  public numUpdateSteps = 0;
+  public minFrameDelay = 0;
+  public running = false;
+  public started = false;
+  public panic = false;
+  public rafHandle?: number;
 
-    /* eslint-disable @typescript-eslint/no-empty-function */
-    public begin = (_time: number, delta: number): void => {};
-    public end = (_fps: number, panic: boolean): void => {};
-    public update = (_delta: number): void => {};
-    public render = (_interpolation: number): void => {};
-    /* eslint-enable @typescript-eslint/no-empty-function */
+  /* eslint-disable @typescript-eslint/no-empty-function */
+  public begin = (_time: number, delta: number): void => {};
+  public end = (_fps: number, panic: boolean): void => {};
+  public update = (_delta: number): void => {};
+  public render = (_interpolation: number): void => {};
+  /* eslint-enable @typescript-eslint/no-empty-function */
 
-    constructor () {
-      this.animate = this.animate.bind(this);
+  constructor () {
+    this.animate = this.animate.bind(this);
+  }
+
+  public get maxAllowedFps (): number {
+    return 1000 / this.minFrameDelay;
+  }
+
+  public set maxAllowedFps (fps: number) {
+    if (fps == null) {
+      fps = Infinity;
     }
-
-    public get maxAllowedFps (): number {
-      return 1000 / this.minFrameDelay;
-    }
-
-    public set maxAllowedFps (fps: number) {
-      if (fps == null) {
-        fps = Infinity;
-      }
         
-      if (fps === 0) {
-        this.stop();
-      } else {
-        this.minFrameDelay = 1000 / fps;
-      }
+    if (fps === 0) {
+      this.stop();
+    } else {
+      this.minFrameDelay = 1000 / fps;
     }
+  }
 
-    public resetFrameDelta (): number {
-      const frameDelta = this.frameDelta;
-      this.frameDelta = 0;
-      return frameDelta;
+  public resetFrameDelta (): number {
+    const frameDelta = this.frameDelta;
+    this.frameDelta = 0;
+    return frameDelta;
+  }
+
+  public start (): void {
+    if (this.started) {
+      return;
     }
-
-    public start (): void {
-      if (this.started) {
-        return;
-      }
         
-      this.started = true;
-      this.rafHandle = requestAnimationFrame((timestamp) => {
-        this.render(1);
-        this.running = true;
-        this.lastFrameTimeMs = timestamp;
-        this.lastFpsUpdate = timestamp;
-        this.framesThisSecond = 0;
-        this.rafHandle = requestAnimationFrame(this.animate);
-      });
-    }
-
-    public stop (): void {
-      this.running = false;
-      this.started = false;
-
-      if (this.rafHandle) {
-        cancelAnimationFrame(this.rafHandle);
-      }
-    }
-
-    public animate (time: number): void {
+    this.started = true;
+    this.rafHandle = requestAnimationFrame((timestamp) => {
+      this.render(1);
+      this.running = true;
+      this.lastFrameTimeMs = timestamp;
+      this.lastFpsUpdate = timestamp;
+      this.framesThisSecond = 0;
       this.rafHandle = requestAnimationFrame(this.animate);
-      if (time < this.lastFrameTimeMs + this.minFrameDelay) {
-        return;
-      }
+    });
+  }
 
-      this.frameDelta += time - this.lastFrameTimeMs;
-      this.lastFrameTimeMs = time;
+  public stop (): void {
+    this.running = false;
+    this.started = false;
 
-      this.begin(time, this.frameDelta);
-
-      if (time > this.lastFpsUpdate + 1000) {
-        this.fps = 0.25 * this.framesThisSecond + 0.75 * this.fps;
-        this.lastFpsUpdate = time;
-        this.framesThisSecond = 0;
-      }
-      ++this.framesThisSecond;
-
-      this.numUpdateSteps = 0;
-      while (this.frameDelta >= this.simulationStep) {
-        this.update(this.simulationStep);
-        this.frameDelta -= this.simulationStep;
-
-        if (++this.numUpdateSteps >= 240) {
-          this.panic = true;
-          break;
-        }
-      }
-
-      this.render(this.frameDelta / this.simulationStep);
-      this.end(this.fps, this.panic);
-
-      this.panic = false;
+    if (this.rafHandle) {
+      cancelAnimationFrame(this.rafHandle);
     }
+  }
+
+  public animate (time: number): void {
+    this.rafHandle = requestAnimationFrame(this.animate);
+    if (time < this.lastFrameTimeMs + this.minFrameDelay) {
+      return;
+    }
+
+    this.frameDelta += time - this.lastFrameTimeMs;
+    this.lastFrameTimeMs = time;
+
+    this.begin(time, this.frameDelta);
+
+    if (time > this.lastFpsUpdate + 1000) {
+      this.fps = 0.25 * this.framesThisSecond + 0.75 * this.fps;
+      this.lastFpsUpdate = time;
+      this.framesThisSecond = 0;
+    }
+    ++this.framesThisSecond;
+
+    this.numUpdateSteps = 0;
+    while (this.frameDelta >= this.simulationStep) {
+      this.update(this.simulationStep);
+      this.frameDelta -= this.simulationStep;
+
+      if (++this.numUpdateSteps >= 240) {
+        this.panic = true;
+        break;
+      }
+    }
+
+    this.render(this.frameDelta / this.simulationStep);
+    this.end(this.fps, this.panic);
+
+    this.panic = false;
+  }
 }
--- project/src/ts/core/vector.ts
+++ project/src/ts/core/vector.ts
@@ -1,122 +1,122 @@
 import BitMath from "./bit-math";
 
 export default class Vector {
-    public x: number;
-    public y: number;
+  public x: number;
+  public y: number;
 
-    /**
+  /**
      * Retrieve total length
      *
      * @returns Total length
      */
-    public get length (): number {
-      return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
-    }
+  public get length (): number {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
 
-    /**
+  /**
      * Constructor
      *
      * @param x - Initial x coordinate
      * @param y - Initial y coordinate
      */
-    constructor (x: number | Vector, y?: number) {
-      if (x instanceof Vector) {
-        this.x = x.x;
-        this.y = x.y;
-      } else {
-        this.x = x;
-        this.y = y ?? x;
-      }
+  constructor (x: number | Vector, y?: number) {
+    if (x instanceof Vector) {
+      this.x = x.x;
+      this.y = x.y;
+    } else {
+      this.x = x;
+      this.y = y ?? x;
     }
+  }
 
-    /**
+  /**
      * Add to coordinates
      *
      * @param x - Added to x coordinate
      * @param y - Added to y coordinate
      * @returns Vector with added coordinates
      */
-    public add (x: number | Vector, y?: number): Vector {
-      if (x instanceof Vector) {
-        return new Vector(this.x + x.x, this.y + x.y);
-      }
-      return new Vector(this.x + x, this.y + (y != null ? y : x));
+  public add (x: number | Vector, y?: number): Vector {
+    if (x instanceof Vector) {
+      return new Vector(this.x + x.x, this.y + x.y);
     }
+    return new Vector(this.x + x, this.y + (y != null ? y : x));
+  }
 
-    public substract (x: number | Vector, y?: number): Vector {
-      if (x instanceof Vector) {
-        return new Vector(this.x - x.x, this.y - x.y);
-      }
-      return new Vector(this.x - x, this.y - (y != null ? y : x));
+  public substract (x: number | Vector, y?: number): Vector {
+    if (x instanceof Vector) {
+      return new Vector(this.x - x.x, this.y - x.y);
     }
+    return new Vector(this.x - x, this.y - (y != null ? y : x));
+  }
 
-    /**
+  /**
      * Floor coordinates
      *
      * @returns Vector with floored coordinates
      */
-    public floor (): Vector {
-      return new Vector(BitMath.floor(this.x), BitMath.floor(this.y));
-    }
+  public floor (): Vector {
+    return new Vector(BitMath.floor(this.x), BitMath.floor(this.y));
+  }
 
-    /**
+  /**
      * Round coordinates
      *
      * @returns Vector with rounded coordinates
      */
-    public round (): Vector {
-      return new Vector(BitMath.round(this.x), BitMath.round(this.y));
-    }
+  public round (): Vector {
+    return new Vector(BitMath.round(this.x), BitMath.round(this.y));
+  }
 
-    /**
+  /**
      * Ceil coordinates
      *
      * @returns Vector with ceiled coordinates
      */
-    public ceil (): Vector {
-      return new Vector(BitMath.ceil(this.x), BitMath.ceil(this.y));
-    }
+  public ceil (): Vector {
+    return new Vector(BitMath.ceil(this.x), BitMath.ceil(this.y));
+  }
 
-    public multiply (x: number | Vector, y?: number): Vector {
-      if (x instanceof Vector) {
-        return new Vector(this.x * x.x, this.y * x.y);
-      }
-      return new Vector(this.x * x, this.y * (y != null ? y : x));
+  public multiply (x: number | Vector, y?: number): Vector {
+    if (x instanceof Vector) {
+      return new Vector(this.x * x.x, this.y * x.y);
     }
+    return new Vector(this.x * x, this.y * (y != null ? y : x));
+  }
 
-    public divide (x: number | Vector, y?: number): Vector {
-      if (x instanceof Vector) {
-        return new Vector(this.x / x.x, this.y / x.y);
-      }
-      return new Vector(this.x / x, this.y / (y != null ? y : x));
+  public divide (x: number | Vector, y?: number): Vector {
+    if (x instanceof Vector) {
+      return new Vector(this.x / x.x, this.y / x.y);
     }
+    return new Vector(this.x / x, this.y / (y != null ? y : x));
+  }
 
-    public abs (): Vector {
-      return new Vector(Math.abs(this.x), Math.abs(this.y));
-    }
+  public abs (): Vector {
+    return new Vector(Math.abs(this.x), Math.abs(this.y));
+  }
 
-    /**
+  /**
      * Rotate clockwise by degrees
      *
      * @param deg - Angle in degrees
      * @returns Rotated vector
      */
-    public rotate (rad: number): Vector {
-      const cos = Math.cos(rad);
-      const sin = Math.sin(rad);
+  public rotate (rad: number): Vector {
+    const cos = Math.cos(rad);
+    const sin = Math.sin(rad);
 
-      return new Vector(
-        BitMath.round(10000 * (this.x * cos - this.y * sin)) / 10000,
-        BitMath.round(10000 * (this.x * sin + this.y * cos)) / 10000
-      );
-    }
+    return new Vector(
+      BitMath.round(10000 * (this.x * cos - this.y * sin)) / 10000,
+      BitMath.round(10000 * (this.x * sin + this.y * cos)) / 10000
+    );
+  }
 
-    public rotateDeg (deg: number): Vector {
-      const rad = -deg * (Math.PI / 180);
-      return this.rotate(rad);
-    }
+  public rotateDeg (deg: number): Vector {
+    const rad = -deg * (Math.PI / 180);
+    return this.rotate(rad);
+  }
 
-    public toString (): string {
-      return `(${this.x.toFixed(1)}, ${this.y.toFixed(1)})`;
-    }
+  public toString (): string {
+    return `(${this.x.toFixed(1)}, ${this.y.toFixed(1)})`;
+  }
 }
